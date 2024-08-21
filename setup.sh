8f9308fa5bd1a5daa1701ae5d12231d942588aa7
antflix <122604451+antflix@users.noreply.github.com>
2024-08-20 19:48:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 PYTORCH_VERSION="2.0.1"
 PYTORCH_CUDA_VERSION="11.7"
 
-TENSORFLOW_VERSION="2.13.*"
-CUDA_VERSION_TENSORFLOW="11.7.1"
-CUDNN_VERSION_TENSORFLOW="11==8.6.0.163"
+#TENSORFLOW_VERSION="2.13.*"
+#CUDA_VERSION_TENSORFLOW="11.7.1"
+#CUDNN_VERSION_TENSORFLOW="11==8.6.0.163"
 
 HISTSIZE="250000"
 
@@ -35,15 +35,15 @@ conda install pytorch=$PYTORCH_VERSION torchvision torchaudio pytorch-cuda=$PYTO
 python pytorch-gpu-test.py
 
 # set up tensorflow environment
-conda create --name tensorflow python=3.11 -y
-conda activate tensorflow
-conda install -c conda-forge cudatoolkit=$CUDA_VERSION_TENSORFLOW -y
-pip install nvidia-cudnn-cu$CUDNN_VERSION_TENSORFLOW tensorflow==$TENSORFLOW_VERSION
-mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-echo 'CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$CUDNN_PATH/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-python tensorflow-gpu-test.py
+#conda create --name tensorflow python=3.11 -y
+#conda activate tensorflow
+#conda install -c conda-forge cudatoolkit=$CUDA_VERSION_TENSORFLOW -y
+#pip install nvidia-cudnn-cu$CUDNN_VERSION_TENSORFLOW tensorflow==$TENSORFLOW_VERSION
+#mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+#echo 'CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+#echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$CUDNN_PATH/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+#source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+#python tensorflow-gpu-test.py
 
 echo "Installed python and tensorflow environments with the following versions:"
 echo ""
